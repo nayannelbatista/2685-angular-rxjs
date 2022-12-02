2726c40d243f0ad2e6b0efd10bef9022ee7f77de
nayannelbatista <nayannelopes.bd@gmail.com>
2022-12-02 09:58:12 -0300

    Vídeo 3.3

--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Livro } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-livro',
@@ -7,10 +8,10 @@ import { Component, Input } from '@angular/core';
 })
 export class LivroComponent {
 
-  @Input() livro: Object;
+  @Input() livro: Livro;
   modalAberto: boolean;
 
-  onModelChange(evento: boolean) {
+  onModalChange(evento: boolean) {
     this.modalAberto = evento;
   }
 }
